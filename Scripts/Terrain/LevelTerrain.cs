4a69fdf6f99fa330ef0c60441d9ab7b0947a6c40
Adrian Hoppe <adrian.hoppe@yahoo.de>
2020-03-12 12:41:41 +0100

    Added some terrain smoothing.

--- Scripts/Terrain/LevelTerrain.cs
+++ Scripts/Terrain/LevelTerrain.cs
@@ -36,7 +36,7 @@ namespace VRGunners.Terrain
                 Material = new Material(Shader.Find("Standard"));
             }
 
-            meshRenderer.sharedMaterial = Material;            
+            meshRenderer.sharedMaterial = Material;
 
             if (meshFilter == null)
             {
@@ -47,7 +47,7 @@ namespace VRGunners.Terrain
 
             }
 
-            if(meshFilter.sharedMesh == null)
+            if (meshFilter.sharedMesh == null)
             {
                 meshFilter.sharedMesh = new Mesh();
             }
@@ -73,7 +73,6 @@ namespace VRGunners.Terrain
             Vector3[] vertices = new Vector3[width * length];
             Vector2[] uvs = new Vector2[width * length];
             int[] triangles = new int[(width - 1) * (length - 1) * 6];
-            int triIndex = 0;
 
             float minHeight = float.MaxValue;
             for (int y = 0; y < length; y++)
@@ -102,6 +101,7 @@ namespace VRGunners.Terrain
                     //calculate triangle indices
                     if (x != width - 1 && y != length - 1)
                     {
+                        int triIndex = (x + y * (width - 1)) * 6;
                         triangles[triIndex] = i;
                         triangles[triIndex + 1] = i + width;
                         triangles[triIndex + 2] = i + width + 1;
@@ -109,7 +109,6 @@ namespace VRGunners.Terrain
                         triangles[triIndex + 3] = i;
                         triangles[triIndex + 4] = i + width + 1;
                         triangles[triIndex + 5] = i + 1;
-                        triIndex += 6;
                     }
 
                     //calculate uvs
@@ -123,14 +122,24 @@ namespace VRGunners.Terrain
                 vertices[i].y -= minHeight;
             }
 
+            UpdateMesh(true, vertices, uvs, triangles);
+        }
+
+        void UpdateMesh(in Vector3[] vertices) => UpdateMesh(false, vertices, null, null);
+        void UpdateMesh(bool clearOldMeshData, in Vector3[] vertices, in Vector2[] uvs, in int[] triangles)
+        {
             //update mesh filter
-            meshFilter.sharedMesh.Clear();
+            if (clearOldMeshData)
+                meshFilter.sharedMesh.Clear();
             meshFilter.sharedMesh.vertices = vertices;
-            meshFilter.sharedMesh.SetUVs(0, uvs);
-            meshFilter.sharedMesh.triangles = triangles;
+            if (uvs != null)
+                meshFilter.sharedMesh.SetUVs(0, uvs);
+            if (triangles != null)
+                meshFilter.sharedMesh.triangles = triangles;
             meshFilter.sharedMesh.RecalculateNormals();
+
             //update mesh collider
-            meshCollider.sharedMesh.vertices = vertices;
+            meshCollider.sharedMesh = meshFilter.sharedMesh;
         }
 
         public void AddExplosionForce(float explosionForce, Vector3 explosionPos, Vector3 explosionDir, float explosionRadius)
@@ -162,16 +171,53 @@ namespace VRGunners.Terrain
                     const float distanceFactor = 2;
                     float strength = Mathf.Pow(1 - normalizedDist, distanceFactor) * explosionForce;
 
-                    vertices[i] += strength * dir.normalized;
+                    vertices[i].y += (strength * dir.normalized).y;
                 }
-            }            
+            }
 
-            //update mesh filter
-            meshFilter.sharedMesh.vertices = vertices;
-            meshFilter.sharedMesh.RecalculateNormals();
-            
-            //update mesh collider
-            meshCollider.sharedMesh = meshFilter.sharedMesh;            
-        }                
+            UpdateMesh(vertices);
+
+            SmoothTerain();
+        }
+
+        public float pullCloserStrength = 0.5f;
+        void SmoothTerain()
+        {
+            //get copy of vertices
+            Vector3[] vertices = meshFilter.sharedMesh.vertices;
+
+            int width = TerrainProperties.Width;
+            int length = TerrainProperties.Length;
+
+            (int, int)[] offsets = new (int, int)[] { (-1, -1), (1, 1), (-1, 1), (1, -1) };
+
+            for (int y = 1; y < length - 1; y++)
+            {
+                for (int x = 1; x < width - 1; x++)
+                {
+                                                            
+                    int i = x + y * width;
+
+                    for (int j = 0; j < offsets.Length; j++)
+                    {
+                        int i2 = (x + offsets[j].Item1) + (y + offsets[j].Item2) * width;
+
+                        Vector3 v = vertices[i2] - vertices[i];
+                        float dist = v.y;
+
+                        if (dist < TerrainProperties.TileSize * 2)
+                            continue;
+
+                        //move closer together
+                        //vertices[i2] += Vector3.up * 0.2f;
+
+                        vertices[i].y += dist * pullCloserStrength;
+                        vertices[i2].y -= dist * pullCloserStrength;
+                    }                    
+                }
+            }
+
+            UpdateMesh(vertices);
+        }
     }
 }
